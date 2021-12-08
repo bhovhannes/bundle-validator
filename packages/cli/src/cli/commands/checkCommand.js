e8d7a31a212924c89ae8f1d8153b2b2237b096a3
Amy Zhao <azhao@adobe.net>
2021-12-08 15:23:25 -0600

    make pattern argument variadic

--- packages/cli/src/cli/commands/checkCommand.js
+++ packages/cli/src/cli/commands/checkCommand.js
@@ -3,13 +3,13 @@ const { check } = require('../../lib/check.js')
 
 const checkCommand = new Command('check')
 checkCommand.description('Checks given JS files for trace occurencies')
-checkCommand.arguments('<pattern>')
+checkCommand.arguments('<pattern...>')
 checkCommand.option(
   '-p, --plugin <names...>',
   'the names of the plugins that contain the checks to run'
 )
-checkCommand.action(async function (patternArgument, options) {
-  pattern = patternArgument.split(',').map((item) => item.trim())
+checkCommand.action(async function (pattern, options) {
+  //pattern = patternArgument.split(',').map((item) => item.trim())
   console.log('Pattern argument:')
   console.log(pattern)
   console.log('Plugin options:')
