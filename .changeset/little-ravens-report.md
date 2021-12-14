a1010ffcc21755ad82be54dec3610a2926b494d4
Hovhannes Babayan <bhovhannes@gmail.com>
2021-12-14 11:34:10 +0400

    test: test publish using changesets 2

--- .changeset/little-ravens-report.md
+++
@@ -1,5 +0,0 @@
----
-'@bundle-validator/cli': patch
----
-
-Increased timeout to be safe and secure.
--- packages/cli/src/lib/runPlugins.js
+++ packages/cli/src/lib/runPlugins.js
@@ -27,7 +27,7 @@ async function runPlugins(files, config) {
   const plugins = loadPlugins(config)
 
   const mocha = new Mocha({
-    timeout: 300000,
+    timeout: 200000,
     reporter: config.reporter || 'spec',
     reporterOptions: config.reporterOptions || {}
   })
